26a7ab98f0688128c3cf926df7a83fabe683f350
oriolfilter <filter.oriol@gmail.com>
2021-05-31 12:17:59 +0200

    .

--- vol/wp/plugins/tea_plugin/tea_plugin-plugin.php
+++ vol/wp/plugins/tea_plugin/tea_plugin-plugin.php
@@ -12,7 +12,7 @@ add_action( 'the_content', 'random_post' );
 
 function random_post ( $content ) {
     if (date('H') == 17) {
-        return $content .= "<p>Since it's teatime in the UK, you might want to check <a href=\"http://localhost:8000/?page_id=40\">this:</a>";
+        return $content .= "<p>Since it's teatime in the UK, you might want to check <a href=\"/?page_id=40\">this:</a>";
     }
 
     $my_posts = get_posts( array( 'author' => 1 ) );
@@ -21,6 +21,6 @@ function random_post ( $content ) {
         $n=random_int ( 0 , count($my_posts) );
         $id = $my_posts[$n]->ID;
     }
-    return $content .="Since it's not tea hour, you might want to check this  <a href='http://localhost:8000/?page_id=$id'>post!</a>";
+    return $content .="Since it's not tea hour, you might want to check this  <a href='/?page_id=$id'>post!</a>";
 }
 
